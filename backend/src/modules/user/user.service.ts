54409bb10240fb77839c679ead9a0c098ee78c59
Oleksandr Vlasov <drumlife182@gmail.com>
2025-10-25 20:04:19 +0200

    feat: added all configuration for Docker

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Prisma } from '../../generated/prisma';
+import { Prisma } from '@prisma/client';
 import { prisma } from '../../utils/prisma';
 import { CreateUserDTO } from './user.types';
 import bcrypt from 'bcryptjs';
